f0de7860ff510ae3a01f80eeb6038fbb26f5a782
Wilson Junior <58340661+wilsonraj-dev@users.noreply.github.com>
2023-05-11 21:58:35 -0300

    Gerando o Token JWT

---
+++ APICatalogo/APICatalogo/Controllers/AutorizaController.cs
@@ -0,0 +1,110 @@
+﻿using APICatalogo.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace APICatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AutorizaController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IConfiguration _configuration;
+
+        public AutorizaController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager,
+                                  IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return $"AutorizaController: Acessado em {DateTime.Now.ToLongDateString()}";
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> RegisterUser([FromBody] UsuarioDTO usuario)
+        {
+            var user = new IdentityUser
+            {
+                UserName = usuario.Email,
+                Email = usuario.Email,
+                EmailConfirmed = true,
+            };
+
+            var result = await _userManager.CreateAsync(user, usuario.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            await _signInManager.SignInAsync(user, false);
+            return Ok(GerarToken(usuario));
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] UsuarioDTO usuario)
+        {
+            var result = await _signInManager.PasswordSignInAsync(usuario.Email,
+                                                                  usuario.Password,
+                                                                  isPersistent: false,
+                                                                  lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return Ok(GerarToken(usuario));
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Login inválido...");
+                return BadRequest(ModelState);
+            }
+        }
+
+        private UsuarioToken GerarToken(UsuarioDTO usuario)
+        {
+            //Define declarações do usuário
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.UniqueName, usuario.Email ?? throw new ArgumentNullException()),
+                new Claim("meuPet", "pipoca"),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            //Gera uma chave com base em um algoritmo simétrico
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration["Jwt:Key"] ?? throw new ArgumentNullException()));
+
+            //Gera uma assinatura digital do token usando o algoritmo Hmac e a chave privada
+            var credenciais = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            //Tempo de expiração do Token
+            var expiracao = _configuration["TokenConfiguration:ExpireHours"];
+            var expiration = DateTime.UtcNow.AddHours(double.Parse(expiracao));
+
+            //Classe que representa um Token JWT e o gera
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: _configuration["TokenConfiguration:Issuer"],
+                audience: _configuration["TokenConfiguration:Audience"],
+                claims: claims,
+                expires: expiration,
+                signingCredentials: credenciais);
+
+            //Retorna os dados com o token e as suas informações
+            return new UsuarioToken()
+            {
+                Authenticated = true,
+                Token = new JwtSecurityTokenHandler().WriteToken(token),
+                Expiration = expiration,
+                Message = "Token JWT OK",
+            };
+        }
+    }
+}
---
+++ APICatalogo/APICatalogo/DTOs/UsuarioDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalogo.DTOs;
+
+public class UsuarioDTO
+{
+    [DataType(DataType.EmailAddress)]
+    public string? Email { get; set; }
+
+    [DataType(DataType.Password)]
+    public string? Password { get; set; }
+
+    [DataType(DataType.Password)]
+    public string? ConfirmPassword { get; set; }
+}
